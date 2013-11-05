bfb98898a3869a6f9376397b0ffd75ef821b51d5
aviflombaum <avi@flombaum.com>
2013-11-05 15:40:30 -0500

    Add artist_name to song#new form

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,16 @@
 class Song < ActiveRecord::Base
-  belong_to :artist
-  
+  belongs_to :artist
+
+  def artist_name=(artist_name)
+    # Deprecated
+    # self.artist = Artist.find_or_create_by_name(artist_name)
+    
+    # Avi likes these two
+    self.artist = Artist.first_or_create(:name => artist_name)
+    # self.artist = Artist.find_or_create_by(:name => artist_name)
+
+    # Avi Doesn't Like This
+    # self.artist = Artist.where(:name => artist_name).first_or_create
+  end
+
 end
